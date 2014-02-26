5ccfe66a7f902920a5c03865eecd1448a5c66eb2
unknown <u23773@BCH200-E21.dtenet.com>
2014-02-26 17:30:15 -0500

    Adding the files to the project

---
+++ src/main/java/org/egbers/home/x10/resources/LightControllerResource.java
@@ -0,0 +1,39 @@
+package org.egbers.home.x10.resources;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.egbers.home.x10.service.LightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Path("light")
+public class LightControllerResource {
+	
+	@Autowired
+	private LightService lightService;
+	
+	@GET
+	@Path("on/{house}/{unit}")
+	@Produces(APPLICATION_JSON)
+	public JSONObject turnOff(@PathParam("house") String houseCode, @PathParam("unit") String unitCode) throws JSONException {
+		boolean success = lightService.turnOn(houseCode, unitCode);
+		
+		JSONObject response = new JSONObject();
+		response.put("success", success);
+		return response;
+	}
+
+	public void setLightService(LightService lightService) {
+		this.lightService = lightService;
+	}
+	
+}
---
+++ src/main/java/org/egbers/home/x10/service/LightService.java
@@ -0,0 +1,15 @@
+package org.egbers.home.x10.service;
+
+public class LightService {
+	public boolean turnOn(String houseCode, String unitCode) {
+		return true;
+	}
+	
+	public boolean turnOff(String houseCode, String unitCode) {
+		return true;
+	}
+	
+	public boolean dim(String houseCode, String unitCode) {
+		return true;
+	}
+}

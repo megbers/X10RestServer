bced0adaf07c66c6669b143b2bed1d2f10f8b9ea
megbers <matt.egbers@cengage.com>
2016-04-27 00:40:05 -0400

    Adding the new windows executor

---
+++ src/main/java/org/egbers/home/x10/service/ActionExecutorCM17A.java
@@ -0,0 +1,17 @@
+package org.egbers.home.x10.service;
+
+import java.io.IOException;
+
+public class ActionExecutorCM17A extends AbstractActionExecutor implements ActionExecutor {
+
+    @Override
+    public void execute(Action action) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        builder.append("cm17a").append(" ")
+                .append("1").append(" ")
+                .append(action.getHouse().toLowerCase())
+                .append(action.getSocket())
+                .append(action.getValue() == 1 ? "on" : "off");
+        call(builder.toString());
+    }
+}
---
+++ src/test/java/org/egbers/home/x10/service/ActionExecutorCM17ATest.java
@@ -0,0 +1,44 @@
+package org.egbers.home.x10.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ActionExecutorCM17ATest {
+    private ActionExecutorCM17A executor;
+    private Runtime rt;
+    private Process process;
+
+    @Before
+    public void setUp() {
+        executor = new ActionExecutorCM17A();
+        rt = mock(Runtime.class);
+        executor.setRuntime(rt);
+        process = mock(Process.class);
+    }
+
+    @Test
+    public void shouldCallActivateWhenPassedOn() throws Exception {
+        String command = "cm17a 1 a1on";
+        Action action = new Action("A", "1", 1);
+        when(rt.exec(command)).thenReturn(process);
+
+        executor.execute(action);
+
+        verify(rt).exec(command);
+    }
+
+    @Test
+    public void shouldCallDeactivateWhenPassedOff() throws Exception {
+        String command = "cm17a 1 a1off";
+        Action action = new Action("A", "1", 0);
+        when(rt.exec(command)).thenReturn(process);
+
+        executor.execute(action);
+
+        verify(rt).exec(command);
+    }
+}
